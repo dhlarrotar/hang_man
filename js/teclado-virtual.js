d7db7e2e9d86205438ffa9c802582e6976cda01e
dhlarrotar <91299939+dhlarrotar@users.noreply.github.com>
2022-03-06 23:12:33 -0500

    Corrección error teclado virtual

--- js/teclado-virtual.js
+++ js/teclado-virtual.js
@@ -5,6 +5,7 @@ function crearTeclado(){
     var abc= abcList.toUpperCase();
     var areaTeclado=document.querySelector("#area-teclado")
    
+    /*Para cada letra de abcList crear una letra virtual*/ 
     for (let letter of abc){
         var letraDiv = document.createElement("div");
         var letra= document.createElement("p");
@@ -14,12 +15,17 @@ function crearTeclado(){
         letra.classList.add("letra-tecla")
         letraDiv.appendChild(letra)
         areaTeclado.appendChild(letraDiv);
-        letraDiv.addEventListener ("click", function(){
-            this.classList.add("fadeOut2")
-            compararLetra(letter)
 
+        /*Crear evento al hacer click en cada letra virtual*/ 
+        
+        ["touchstart","click"].forEach(function(e) {
+            letra.addEventListener(e, function(){
+                this.classList.remove("letra-tecla");
+                this.classList.add("fadeOut2");
+                this.textContent="-";
+                compararLetra(letter); 
+            });
         });
-    
-    }
+    };
 }
 
