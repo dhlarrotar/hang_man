59174b35b310a39e1c0301ccbbdc1193d6c23650
dhlarrotar <91299939+dhlarrotar@users.noreply.github.com>
2022-05-11 12:45:07 -0500

    update words and hints

--- js/palabras-dict.js
+++ js/palabras-dict.js
@@ -5,20 +5,22 @@ var palabrasDictOtros;
 
 
 var palabrasDictMineralesEs = {
-    "Cuarzo": "Soy resistente y abundante",
-    "Calcita": "Soy un Carbonato de Calcio muy estable",
-    "Diamante": "Puedo ser de carbono pero soy muy valioso",
-    "Olivino": "Soy un silicato y me altero fácilmente",
+    "Cuarzo": "La mayoría de las rocas contienen al menos una pequeña cantidad de este mineral. Sus cristales se utilizan como piedras preciosas o como componente de dispositivos electrónicos debido a sus propiedades piezoeléctricas.", 
+    "Calcita": "Es el principal constituyente de la caliza y el mármol con una amplia gama de usos para la construcción, el secuestro de CO2, el tratamiento del suelo y más.",
+    "Diamante": "Es la sustancia más dura conocida con el punto de fusión más alto (4090 °C). A pesar de su densa configuración atómica, es transparente en un gran número de longitudes de onda.", 
+    "Olivino": "Los miembros de este grupo son uno de los principales minerales formadores de rocas y el componente principal del manto superior de la Tierra. Se utilizan para aplicaciones metalúrgicas o como piedras preciosas.",
 }
 
 var palabrasDictAnimalesEs = {
-    "elefante": "Tengo trompa, orejas grandes y colmillos",
-    "jirafa": " Tengo cuello largo",
-    "murcielago": "Me gustan las cuevas y la oscuridad",
-    "abeja": "Me encantan las flores y produzco miel",
-    "perro":"Soy el mejor amigo del hombre",
-    "pulpo": "Tengo tentáculos",
+    "elefante": "Soy el mamífero terrestre más grande del mundo con quizás el órgano multifuncional más sensible. Podemos comunicarnos a través de señales sísmicas.",
+    "jirafa": "Mi altura de hasta 5,5 metros y mi excelente vista me ayudan a estar atento a los depredadores desde lejos. Arranco fácilmente las hojas con mi larga lengua.",
+    "murcielago": "Como insectos y polinizo cientos de especies de plantas. Mis ojos adaptados a condiciones de poca luz y la ecolocalización me ayudan a ver el mundo.",
+    "abeja": "¡Nos encanta el trabajo en equipo! ¡Cuando encuentro una buena flor para recolectar polen y néctar, comunico la ubicación a mis compañeros usando movimientos especiales de baile!",
+    "perro":"La huella de mi nariz es única y mi sentido del olfato es legendario, con hasta 300 millones de receptores (mientras que un humano tiene alrededor de 5 millones).",
+    "pulpo": "Soy un cefalópodo altamente inteligente, un excelente nadador y un maestro para la mímica. Tengo 3 corazones.",
 }
+
+
 
 var palabrasDictOtrosEs = {
     "luna":"Giro alrededor de la tierra y salgo de noche",
@@ -29,21 +31,22 @@ var palabrasDictOtrosEs = {
 
 
 var palabrasDictMinerals = {
-    "Quartz": "I'm hardy and abundant", 
-    "Calcite": "Polymorph of Calcium Carbonate", 
-    "Diamond": "Made of Carbon but highly valuable", 
-    "Olivine": "Easy-altered green silicate"
+    "Quartz": "The majority of rocks contain at least a small amount of this mineral. Their crystals are used as gemstones or as a component of electronic devices due to their piezoelectric properties.",
+    "Calcite": "It is the principal constituent of limestone and marble with a wide range of uses for construction, CO2 sequestration, soil treatment, and more.",
+    "Diamond": "It is the hardest known substance with the highest melting point(4090° C). Despite its dense atomical configuration, it is transparent over a great number of wavelengths.",  
+    "Olivine": "Members of this group are major rock-forming minerals and the primary component of the Earth's upper mantle. They are used for metalworking applications or as gemstones.", 
+
     
 
 }
 
 var palabrasDictAnimals = {
-    "elephant": "I have a trunk", 
-    "giraffe": "I have a long neck", 
-    "bat": "I like caves", 
-    "bee": "I love pollen", 
-    "dog":"I'm human's best friend", 
-    "octopus": "I have tentacles",
+    "elephant": "I'm the world's largest land mammal with perhaps the most sensitive multifunctional organ. We can communicate through seismic signals.",
+    "giraffe": "My height (up to 5.5 meters!) and excellent eyesight help me to keep a lookout for predators from far away. I easily pull down leaves with my long tongue.",
+    "bat": "I eat insects and pollinate hundreds of plant species. My eyes attuned to low-light conditions and echolocation help me to see the world.",
+    "bee": "We love teamwork! when I find a good flower to collect pollen and nectar, I communicate the location to my mates using special dancing moves!",
+    "dog":"My nose print is unique and my sense of smell legendary, with as many as 300 million receptors (while a human has about 5 million.)",  
+    "octopus": "I am a highly intelligent cephalopod, an excellent swimmer, and a master of mimics. I have 3 hearts.",
 }
 
 var palabrasDictOther = {
